4109f452b1bac01a355510bdfa09f3843f708b12
Spruce Bondera <sprucebondera@gmail.com>
2016-09-04 21:50:04 -0400

    Fix vector declaration bug

--- vector.h
+++ vector.h
@@ -19,6 +19,63 @@ typedef uint8_t* bytes;
                                                                         \
   int append_##N(N##_vector *vector, T value);
 
+#define DEFINE_VECTOR(T, N)                                             \
+                                                                        \
+  N##_vector new_##N##_vector(size_t length) {                          \
+    N##_vector new_vec;                                                 \
+    new_vec.data = malloc(sizeof(T) * length);                          \
+    new_vec.length = length;                                            \
+    new_vec.capacity = length;                                          \
+    return new_vec;                                                     \
+  }                                                                     \
+                                                                        \
+  T get_##N(N##_vector *vector, int index) {                            \
+    if (index >= vector->length || (index < 0) &&                       \
+        (-index) > vector->length) {                                    \
+      fprintf(stderr,                                                   \
+              "Index %d is out of array bounds (length %zd)\n",         \
+              index, vector->length);                                   \
+      return *(vector->data);                                           \
+    }                                                                   \
+    if (index >= 0) {                                                   \
+      return vector->data[index];                                       \
+    } else {                                                            \
+      return vector->data[vector->length + index];                      \
+    }                                                                   \
+  }                                                                     \
+                                                                        \
+  void set_##N(N##_vector *vector, int index, T value) {                \
+    if (index >= vector->length || (index < 0) &&                       \
+        (-index) > vector->length) {                                    \
+      fprintf(stderr,                                                   \
+              "Index %d is out of array bounds (length %zd)\n",         \
+              index, vector->length);                                   \
+      return;                                                           \
+    }                                                                   \
+    if (index >= 0) {                                                   \
+      vector->data[index] = value;                                      \
+    } else {                                                            \
+      vector->data[vector->length - index] = value;                     \
+    }                                                                   \
+  }                                                                     \
+                                                                        \
+  int append_##N(N##_vector *vector, T value) {                         \
+    if (vector->length >= vector->capacity) {                           \
+      T *realloced = realloc(vector->data,                              \
+                             (vector->capacity + 1) * 2 * sizeof(T));   \
+      if (!realloced) {                                                 \
+        free(vector->data);                                             \
+        return -1;                                                      \
+      }                                                                 \
+      vector->data = realloced;                                         \
+      vector->capacity = (vector->capacity + 1) * 2;                    \
+    }                                                                   \
+    vector->data[vector->length] = value;                               \
+    vector->length++;                                                   \
+    return vector->length;                                              \
+  }
+
+
 DEFINE_VECTOR_HEADER(int, int)
 DEFINE_VECTOR_HEADER(char *, string)
 DEFINE_VECTOR_HEADER(double, double)
@@ -31,14 +88,14 @@ DEFINE_VECTOR_HEADER(size_t, size_t)
 #define get(X, I) _Generic((X),                                         \
                              int_vector *: get_int,                     \
                            string_vector *: get_string,                 \
-                           double_vector *: get_double,                 \
+                             double_vector *: get_double,               \
                            float_vector *: get_float,                   \
-                           data_vector *: get_data,                     \
+                             data_vector *: get_data,                   \
                            char_vector *: get_char,                     \
-                           size_t_vector *: get_size_t,                 \
+                             size_t_vector *: get_size_t,               \
                            byte_vector *: get_byte,                     \
-  default: get_int                                                      \
-                           )(X, I)
+ default: get_int                                                       \
+                             )(X, I)
 
 
 #define set(X, I, V) _Generic((X),                                      \
@@ -51,7 +108,7 @@ DEFINE_VECTOR_HEADER(size_t, size_t)
                               size_t_vector *: set_size_t,              \
                               byte_vector *: set_byte,                  \
   default: set_int                                                      \
-                              )(X, I, V)
+                                )(X, I, V)
 
 
 #define append(X, V) _Generic((X),                                      \
